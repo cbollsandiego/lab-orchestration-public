edc0c11bec7363d9b1379e16ee5864a4229cb28d
Connor Boll <cboll@sandiego.edu>
2023-07-13 09:37:21 -0700

    Initial navbar created

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/usd-logo-primary-thumb.png" alt="" width="72" height="42" class="d-inline-block align-text-top">
+            </a>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">My Courses</a>
+              <a class="nav-link" href="#">Courses</a>
+              <a class="nav-link" href="#">Users</a>
+              <router-link :to="{name: 'Create Lab'}">
+                <a class="nav-link">Labs</a>
+              </router-link>
+            </div>
+          </div>
+          <button class="btn btn-outline-success" type="submit">Login</button>
+        </div>
+      </nav>
+</template>
+
+<script>
+import { useLink } from 'vue-router';
+
+export default {
+    
+}
+</script>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+</style>
