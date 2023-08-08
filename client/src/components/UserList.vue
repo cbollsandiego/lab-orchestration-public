62dfbcbe48ca30b7052884aea238c64aae97c9cd
Connor Boll <cboll@sandiego.edu>
2023-08-08 09:15:31 -0700

    redirect if not logged in on user list page

--- client/src/components/UserList.vue
+++ client/src/components/UserList.vue
@@ -115,6 +115,7 @@ export default {
                 })
                 .catch((error) => {
                     console.error(error);
+                    this.$router.push({ name: 'Login'});
                 });
         },
         handleDeleteUser(user) {
